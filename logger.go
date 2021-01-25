fbf57c6e7a1ba3d41ee7dd8c3d9afd6021e0ffc5
Michael Welles <mlwelles@gmail.com>
2021-01-25 13:17:48 -0500

    added with to be able to use context sugared loggers

--- logger.go
+++ logger.go
@@ -31,6 +31,8 @@ type Logger interface {
 	Fatalw(msg string, keysAndValues ...interface{})
 	StdLogger() *log.Logger
 	ZapLogger() *zap.Logger
+	ZapSugaredLogger() *zap.SugaredLogger
+	With(args ...interface{}) Logger
 }
 
 type Option func(*logger)
@@ -41,12 +43,31 @@ func WithZap(zapLogger *zap.Logger) Option {
 	}
 }
 
+func WithZapSugared(sugar *zap.SugaredLogger) Option {
+	return func(l *logger) {
+		l.SugaredLogger = sugar
+		if l.SugaredLogger == nil {
+			l.zapLogger = nil
+		} else {
+			l.zapLogger = sugar.Desugar()
+		}
+	}
+}
+
 type logger struct {
 	*zap.SugaredLogger
 	zapLogger *zap.Logger
 	stdLogger *log.Logger
 }
 
+func (l logger) With(args ...interface{}) Logger {
+	sl := l.ZapSugaredLogger().With(args...)
+	return NewLogger(WithZapSugared(sl))
+}
+
+func (l logger) ZapSugaredLogger() *zap.SugaredLogger {
+	return l.SugaredLogger
+}
 func (l logger) ZapLogger() *zap.Logger {
 	return l.zapLogger
 }
@@ -66,7 +87,6 @@ func NewLogger(options ...Option) Logger {
 		atomicLevel := zap.NewAtomicLevel()
 		atomicLevel.SetLevel(zapcore.ErrorLevel)
 		l.zapLogger = zap.New(zapcore.NewCore(encoder, zapcore.Lock(os.Stderr), atomicLevel))
-
 	}
 	l.SugaredLogger = l.zapLogger.Sugar()
 	l.stdLogger = zap.NewStdLog(l.zapLogger)
